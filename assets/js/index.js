0d5fe9540ac5ed757c865f8d869a83dde7c86c33
GLira4k <103190663+GLira4k@users.noreply.github.com>
2024-01-17 18:11:16 -0300

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 const btn = document.getElementById("menu-button");
 const menu = document.getElementById("menu");
+const particlesjs = document.getElementById("#particles-js");
 
 btn.addEventListener("click", () => {
     menu.classList.toggle("hidden")
@@ -12,3 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
         once: false, // A animação ocorrerá apenas uma vez
     });
 });
+
+particlesjs.load('particles-js', '../particlesjs-config.json', function() {
+    console.log('callback - particles-js config loaded');
+  });
