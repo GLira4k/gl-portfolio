712d33c6dbe96cd16c1165b6316c2e0b0bde0b18
GLira4k <103190663+GLira4k@users.noreply.github.com>
2024-01-17 11:47:01 -0300

    Finalizando html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,9 @@
 const btn = document.getElementById("menu-button");
 const menu = document.getElementById("menu");
 
-
 btn.addEventListener("click", () => {
     menu.classList.toggle("hidden")
 });
+
+
 
